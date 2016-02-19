adee1ea67d6c74c96eb5168459c080ff8f406164
marcemc <marcemc@yahoo.com>
2016-02-18 22:08:51 -0500

    Just now set mailer url to localhost

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,7 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  config.action_mailer.default_url_options = { host: '127.0.0.1', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
 
 
